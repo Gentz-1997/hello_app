7c27dd86668cf956f2f98d9f94723ed970591b62
Genki Matsumi <125166664+Gentz-1997@users.noreply.github.com>
2024-10-03 04:42:59 +0000

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  def hello
+    render html: "Hello, World!"
+  end
+
+  def hola
+    render html: "hola, mundo!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!"
+  end
+
 end
